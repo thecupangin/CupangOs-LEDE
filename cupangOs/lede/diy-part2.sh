f11cbeda92055033961c80c5b18e6a9ad68d586e
robbyaprianto <robbyaprianto@hotmail.com>
2022-02-24 02:30:40 +0700

    update 24feb2022

--- cupangOs/lede/diy-part2.sh
+++ cupangOs/lede/diy-part2.sh
@@ -8,7 +8,7 @@
 # ------------------------------- Main source started -------------------------------
 #
 # Modify default theme（FROM uci-theme-bootstrap CHANGE TO luci-theme-neobird）
-sed -i 's/luci-theme-bootstrap/luci-theme-neobird/g' ./feeds/luci/collections/luci/Makefile
+sed -i 's/luci-theme-bootstrap/luci-theme-argon/g' ./feeds/luci/collections/luci/Makefile
 
 # Add autocore support for armvirt
 sed -i 's/TARGET_rockchip/TARGET_rockchip\|\|TARGET_armvirt/g' package/lean/autocore/Makefile
@@ -18,7 +18,7 @@ sed -i "s|DISTRIB_REVISION='.*'|DISTRIB_REVISION='R$(date +%Y.%m.%d)'|g" package
 echo "DISTRIB_SOURCECODE='lede'" >>package/base-files/files/etc/openwrt_release
 
 # Modify default IP（FROM 192.168.1.1 CHANGE TO 192.168.31.4）
-sed -i 's/192.168.1.1/12.12.12.1/g' package/base-files/files/bin/config_generate
+#sed -i 's/192.168.1.1/12.12.12.1/g' package/base-files/files/bin/config_generate
 
 # change ssid
 sed -i "s/OpenWrt/CupangOs/g" package/kernel/mac80211/files/lib/wifi/mac80211.sh
@@ -30,8 +30,8 @@ sed -i "s/OpenWrt/CupangOs/g" package/base-files/files/bin/config_generate
 sed -i "s/UTC/WIB-7/g" package/base-files/files/bin/config_generate
 
 # Modify default root's password（FROM 'password'[$1$V4UetPzk$CYXluq4wUazHjmCDBCqXF.] CHANGE TO 'your password'）
-sed -i 's/root::0:0:99999:7:::/root:$1$wOheR5zg$7LWQZUwOPIkCB8M9WoTfo1:0:0:99999:7:::/g' /etc/shadow
-popd
+#sed -i 's/root::0:0:99999:7:::/root:$1$wOheR5zg$7LWQZUwOPIkCB8M9WoTfo1:0:0:99999:7:::/g' /etc/shadow
+#popd
 
 # Modify default IP（FROM 192.168.1.1 CHANGE TO 192.168.31.4）
 #sed -i 's/192.168.1.1/192.168.100.1/g' package/base-files/files/bin/config_generate
@@ -63,7 +63,7 @@ svn co https://github.com/ophub/luci-app-amlogic/trunk/luci-app-amlogic package/
 svn co https://github.com/thecupangin/cupangOs-packages/trunk/luci-app-openclash package/luci-app-openclash
 
 # rooter system
-git clone https://github.com/ofmodemsandmen/RooterSource source
+#git clone https://github.com/ofmodemsandmen/RooterSource source
 
 #add tema
 git clone --dept 1 https://github.com/thinktip/luci-theme-neobird.git package/luci-theme-neobird
@@ -75,10 +75,6 @@ git clone https://github.com/thecupangin/luci-theme-argon_armygreen package/lean
 git clone --depth=1 -b 18.06 https://github.com/jerrykuku/luci-theme-argon
 git clone --depth=1 https://github.com/jerrykuku/luci-app-argon-config
 rm -rf ../lean/luci-theme-argon
-# Add luci-app-diskman
-git clone --depth=1 https://github.com/SuLingGG/luci-app-diskman
-mkdir parted
-cp luci-app-diskman/Parted.Makefile parted/Makefile
 
 #
 # ------------------------------- Other ends -------------------------------
